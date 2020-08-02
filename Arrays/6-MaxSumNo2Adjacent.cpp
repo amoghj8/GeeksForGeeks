a39a7242cdfb49bd1991d9a573b0b49d7dabd268
Amogh Joshi <amoghj8@gmail.com>
2020-08-03 01:45:32 +0530

    Fix-Maximum sum so that no 2 elements are adjacent

--- Arrays/6-MaxSumNo2Adjacent.cpp
+++ Arrays/6-MaxSumNo2Adjacent.cpp
@@ -7,11 +7,12 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    vector<int> array ={1,20,3};
+    vector<int> array ={4,1,1,4,2,1};
     int maxSum=INT_MIN;
     for( int i=0;i<2;i++) {
-        int temp_Sum = array[i];
-        for(int j=i+2,k=j;j<i+4;j++) {
+        for(int j=i+2;j<i+4;j++) {
+            int temp_Sum = array[i];
+            int k=j;
             while(k<array.size()) {
                 temp_Sum += array[k];
                 k+=2;
